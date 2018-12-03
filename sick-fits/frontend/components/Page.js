bd8b4428dca69a94049dd25d1a92996e83874730
lenmorld <lenmorld@gmail.com>
2018-12-02 19:30:50 -0500

    Header + Nav components

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Header from './Header';
 
 class Page extends Component {
     render() {
         return (
             <div>
-                <p>Hey I'm the page component</p>
+                <Header/>
+                {/* <p>Hey I'm the page component</p> */}
                 {this.props.children}
             </div>
         );
--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -4,11 +4,6 @@ import Link from 'next/link';
 const Home = props => (
     <div>
         <p>Hey!</p>
-        {/* <a href="/sell">Sell</a> */}
-
-        <Link href="/sell">
-            <a>Sell!</a>
-        </Link>
     </div>
 )
 
