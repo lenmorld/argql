b2434b59f9e0af4bb451dbe88f5b8f136b22469a
lenmorld <lenmorld@gmail.com>
2018-12-02 19:35:45 -0500

    Meta components for head meta tags

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import Meta from './Meta';
 
 class Page extends Component {
     render() {
         return (
             <div>
+                <Meta />
                 <Header/>
                 {/* <p>Hey I'm the page component</p> */}
                 {this.props.children}
