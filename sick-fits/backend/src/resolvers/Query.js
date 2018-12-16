7e3a6a50b5607bcf6adedb50a112642e11978440
lenmorld <lenmorld@gmail.com>
2018-12-16 18:31:17 -0500

    3_14_c_items Query

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -10,6 +10,12 @@ const Query = {
     //     global.dogs = global.dogs || [];
     //     return global.dogs;
     // }
+
+    async items(parent, args, ctx, info) {
+        const items = await ctx.db.query.items();
+        console.log(items);
+        return items;
+    },
 };
 
 module.exports = Query;
