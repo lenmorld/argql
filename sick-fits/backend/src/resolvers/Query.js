948c49aabd7e83a0f7ce0bc9c1468897625c912d
lenmorld <lenmorld@gmail.com>
2018-12-16 18:18:56 -0500

    3_14_b_createItem Mutation

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,15 +1,15 @@
 const Query = {
-    dogs(parent, args, ctx, info) {
+    // dogs(parent, args, ctx, info) {
 
-        // TODO: pull data from DB (Prisma)
-        // return [
-        //     { name: 'Snickers' }, 
-        //     { name: 'Sunny' }
-        // ];
+    //     // TODO: pull data from DB (Prisma)
+    //     // return [
+    //     //     { name: 'Snickers' }, 
+    //     //     { name: 'Sunny' }
+    //     // ];
 
-        global.dogs = global.dogs || [];
-        return global.dogs;
-    }
+    //     global.dogs = global.dogs || [];
+    //     return global.dogs;
+    // }
 };
 
 module.exports = Query;
