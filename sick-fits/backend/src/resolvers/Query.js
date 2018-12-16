ffd6eeabfea2e105444d9bd2b46b52a8170d5ba5
lenmorld <lenmorld@gmail.com>
2018-12-16 17:09:26 -0500

    3_13_First query hard coded Dogs

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,12 @@
-const Query = {};
+const Query = {
+    dogs(parent, args, ctx, info) {
+
+        // TODO: pull data from DB (Prisma)
+        return [
+            { name: 'Snickers' }, 
+            { name: 'Sunny' }
+        ];
+    }
+};
 
 module.exports = Query;
