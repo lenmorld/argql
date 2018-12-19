4f354d636b682677f6465cf985f44a6db439b6cd
lenmorld <lenmorld@gmail.com>
2018-12-18 21:46:00 -0500

    4_17 CreateItem Form

--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import CreateItem from '../components/CreateItem';
+
 const Sell = props => (
     <div>
-        <p>Sell!</p>
+        <CreateItem />
     </div>
 )
 
