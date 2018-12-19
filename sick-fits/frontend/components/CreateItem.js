3d649ca77f0c367dbd4997909c76bd1a663b9e17
lenmorld <lenmorld@gmail.com>
2018-12-18 22:18:05 -0500

    4_17 Mutation to Gql, mutation component w/ render props, error and loading

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -4,6 +4,29 @@ import gql from "graphql-tag";
 
 import Form from "./styles/Form";
 import formatMoney from "../lib/formatMoney";
+import Error from "./ErrorMessage";
+
+// mutation query
+// createItem from `schema.graphql`
+const CREATE_ITEM_MUTATION = gql`
+  mutation CREATE_ITEM_MUTATION(
+    $title: String!
+    $description: String!
+    $price: Int!
+    $image: String
+    $largeImage: String
+  ) {
+    createItem(
+      title: $title
+      description: $description
+      price: $price
+      image: $image
+      largeImage: $largeImage
+    ) {
+      id
+    }
+  }
+`;
 
 class CreateItem extends Component {
   state = {
@@ -27,53 +50,59 @@ class CreateItem extends Component {
   };
   render() {
     return (
-      <Form
-        onSubmit={e => {
-          e.preventDefault();
-          console.log(this.state);
-        }}
-      >
-        <fieldset>
-          <label htmlFor="title">
-            Title
-            <input
-              type="text"
-              id="title"
-              name="title"
-              placeholder="Title"
-              required
-              value={this.state.title}
-              onChange={this.handleChange}
-            />
-          </label>
-          <label htmlFor="price">
-            Price
-            <input
-              type="number"
-              id="price"
-              name="price"
-              placeholder="Price"
-              required
-              value={this.state.price}
-              onChange={this.handleChange}
-            />
-          </label>
-          <label htmlFor="description">
-            Description
-            <textarea
-              id="description"
-              name="description"
-              placeholder="Enter a description"
-              required
-              value={this.state.description}
-              onChange={this.handleChange}
-            />
-          </label>
-          <button type="submit">Submit</button>
-        </fieldset>
-      </Form>
+      <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
+        {(createItem, { loading, error }) => (
+          <Form
+            onSubmit={e => {
+              e.preventDefault();
+              console.log(this.state);
+            }}
+          >
+            <Error error={error} />
+            <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="title">
+                Title
+                <input
+                  type="text"
+                  id="title"
+                  name="title"
+                  placeholder="Title"
+                  required
+                  value={this.state.title}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <label htmlFor="price">
+                Price
+                <input
+                  type="number"
+                  id="price"
+                  name="price"
+                  placeholder="Price"
+                  required
+                  value={this.state.price}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <label htmlFor="description">
+                Description
+                <textarea
+                  id="description"
+                  name="description"
+                  placeholder="Enter a description"
+                  required
+                  value={this.state.description}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <button type="submit">Submit</button>
+            </fieldset>
+          </Form>
+        )}
+      </Mutation>
     );
   }
 }
 
 export default CreateItem;
+export { CREATE_ITEM_MUTATION };
