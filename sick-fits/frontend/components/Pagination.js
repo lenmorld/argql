e8e7de3ff305e0595be8e02cf143f45a35a86192
lenmorld <lenmorld@gmail.com>
2018-12-27 21:03:19 -0500

    4_22_g_pagination next prev links

--- sick-fits/frontend/components/Pagination.js
+++ sick-fits/frontend/components/Pagination.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import gql from 'graphql-tag';
-import { Query } from 'react-apollo';
-import {perPage} from '../config';
 import Head from 'next/head';
+import Link from 'next/link';
+
+import { Query } from 'react-apollo';
+import { perPage } from '../config';
 import PaginationStyles from './styles/PaginationStyles';
 
 const PAGINATION_QUERY = gql`
@@ -29,12 +31,41 @@ const Pagination = props => (
 							Sick Fits! — Page {page} of {pages}
 						</title>
 					</Head>
+					<Link
+						prefetch
+						href={{
+							pathname: 'items',
+							query: { page: page - 1 },
+						}}
+					>
+						<a
+							className="prev"
+							aria-disabled={page <= 1}
+						>⬅ Prev
+      			</a>
+					</Link>
 					<p>
 							Page {page} of {pages}
 					</p>
+					<p>
+						{count} Items Total
+					</p>
+					<Link
+						prefetch
+						href={{
+							pathname: 'items',
+							query: { page: page + 1 },
+						}}
+					>
+						<a
+							className="prev"
+							aria-disabled={page >= pages}
+						>Next ➡
+      			</a>
+					</Link>
 				</PaginationStyles>
 			);
- }
+		}
 		}
 	</Query>
 );
