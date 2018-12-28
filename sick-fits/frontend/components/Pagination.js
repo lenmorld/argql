e5472f371d0a789c8836d9e3e4f0d8cbc71da84a
lenmorld <lenmorld@gmail.com>
2018-12-27 20:21:00 -0500

    4_22_c_pagination frontend query

--- sick-fits/frontend/components/Pagination.js
+++ sick-fits/frontend/components/Pagination.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import PaginationStyles from './styles/PaginationStyles';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+const PAGINATION_QUERY = gql`
+	query PAGINATION_QUERY {
+		itemsConnection {
+			aggregate {
+				count
+			}
+		}
+	}
+`;
 
 const Pagination = props => (
 	<PaginationStyles>
-		<p>pagination</p>
+		<Query query={PAGINATION_QUERY}>
+			{({ data, loading, error }) => {
+				if (loading) return <p>Loading...</p>
+				return (
+				<p>pagination {data.itemsConnection.aggregate.count}</p>
+			)}
+		}
+		</Query>
 	</PaginationStyles>
 )
 
