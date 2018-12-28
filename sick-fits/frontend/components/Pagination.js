d17af551a252aaa68726189fa5cf3c6a80de7ecc
lenmorld <lenmorld@gmail.com>
2018-12-27 20:41:47 -0500

    4_22_f_pagination title bar via Head

--- sick-fits/frontend/components/Pagination.js
+++ sick-fits/frontend/components/Pagination.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import PaginationStyles from './styles/PaginationStyles';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import {perPage} from '../config';
+import Head from 'next/head';
+import PaginationStyles from './styles/PaginationStyles';
 
 const PAGINATION_QUERY = gql`
 	query PAGINATION_QUERY {
@@ -15,20 +16,27 @@ const PAGINATION_QUERY = gql`
 `;
 
 const Pagination = props => (
-	<PaginationStyles>
-		<Query query={PAGINATION_QUERY}>
-			{({ data, loading, error }) => {
-				if (loading) return <p>Loading...</p>;
-				const count = data.itemsConnection.aggregate.count;
-				const pages = Math.ceil(count / perPage);
-				return (
-				<p>
-					Page {props.page} of {pages}
-				</p>
-			)}
+	<Query query={PAGINATION_QUERY}>
+		{({ data, loading, error }) => {
+			if (loading) return <p>Loading...</p>;
+			const count = data.itemsConnection.aggregate.count;
+			const pages = Math.ceil(count / perPage);
+			const page = props.page;
+			return (
+				<PaginationStyles>
+					<Head>
+						<title>
+							Sick Fits! — Page {page} of {pages}
+						</title>
+					</Head>
+					<p>
+							Page {page} of {pages}
+					</p>
+				</PaginationStyles>
+			);
+ }
 		}
-		</Query>
-	</PaginationStyles>
-)
+	</Query>
+);
 
 export default Pagination;
